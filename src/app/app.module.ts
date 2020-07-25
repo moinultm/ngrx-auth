2ddd31c8c3c64b26673a0b88317a706efadcf212
moinultm <moinultm@gmail.com>
2020-07-25 21:45:01 +0600

    Install NGRX Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'log-in', component: LogInComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: '', component: LandingComponent },
+      { path: '**', redirectTo: '/' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
